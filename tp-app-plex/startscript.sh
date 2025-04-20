00ec3ddb83c0520e8644bf3d4a86afc04a4de11d
Deb Mishra <debmishra@daedal.org>
2025-04-21 00:18:09 +0200

    first draft ready to test. plex media server containerized

---
+++ tp-app-plex/startscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#This scripts sets the welcome video
+# Ensure the /welcome directory exists
+mkdir -p /welcome
+
+# Move files matching *welcome.mp4 if they exist
+shopt -s nullglob
+for file in *welcome.mp4; do
+  mv "$file" /welcome
+done
+shopt -u nullglob
+# This script is used to start the Plex Media Server in a Docker container.
+exec /usr/lib/plexmediaserver/"Plex Media Server"
