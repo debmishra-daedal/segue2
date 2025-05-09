8e376f8306c07ebf4277683c66820f4309b1b2b7
Deb Mishra <infra@daedal.org>
2025-05-09 20:41:41 +0200

    modified:   tp-app-plex/startscript.sh

--- tp-app-plex/startscript.sh
+++ tp-app-plex/startscript.sh
@@ -9,5 +9,7 @@ for file in *welcome.mp4; do
   mv "$file" /welcome
 done
 shopt -u nullglob
+#kill plex media server
+pkill -f "Plex Media Server"
 # This script is used to start the Plex Media Server in a Docker container.
 exec /usr/lib/plexmediaserver/"Plex Media Server"
